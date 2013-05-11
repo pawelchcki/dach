b2fcb4ed951ee7d5980262976e75f20cbc9765b3
Pawel Chojnacki <pawel@chojnacki.ws>
2013-05-11 16:35:12 +0200

    enable server to send files in current directory as well as JAR file

--- src/dach/FileServer.java
+++ src/dach/FileServer.java
@@ -1,17 +1,38 @@
 package dach;
+
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 
 public class FileServer extends org.eclipse.jetty.server.Server {
-	private ResourceHandler resourceHandler;
+    ResourceHandler _resourceHandler;
+    ResourceHandler _fileHandler;
+    ContextHandler _bindHandler;
+    HandlerList _handlerList;
 	
 	FileServer(){
-		super(4000);	
-		String serverRoot = ClassLoader.getSystemResource("dach/resources").toExternalForm();
+        super(4000);
+        // Used for serving static content from jar file
+        String serverResourcesRoot = ClassLoader.getSystemResource("dach/resources/web/")
+                .toExternalForm();
+        String serverFilesRoot = "./";
+        _resourceHandler = new ResourceHandler();
+        _resourceHandler.setDirectoriesListed(true);
+        _resourceHandler.setResourceBase(serverResourcesRoot);
+        // Used for serving content from CWD
+        _fileHandler = new ResourceHandler();
+        _fileHandler.setResourceBase(serverFilesRoot);
+        _fileHandler.setDirectoriesListed(true);
 
-		resourceHandler = new ResourceHandler();
-		resourceHandler.setDirectoriesListed(true);
-		resourceHandler.setResourceBase(serverRoot);
-		
-		this.setHandler(resourceHandler);
+        _bindHandler = new ContextHandler();
+        _bindHandler.setContextPath("/root");
+        _bindHandler.setHandler(_fileHandler);
+        
+
+        _handlerList = new HandlerList();
+        _handlerList.addHandler(_bindHandler);
+        _handlerList.addHandler(_resourceHandler);
+
+        this.setHandler(_handlerList);
 	}
 }
